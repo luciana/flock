a6a42702c98a8d0c60d0d046815a2d478c4c7e4d
Luciana Bruscino <luciana.bruscino@gmail.com>
2023-02-05 17:43:24 -0500

    changes to main

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -1,16 +1,18 @@
 import React, { useContext } from 'react';
 import '../pages.css';
 import './../profile-nav.css';
-
+import { AppContext} from '../../Contexts';
 import Questions from '../../Components/Questions/Questions';
-
+import { Alert } from '../../Components';
 
 function Main() {  
- 
+  const { state } = useContext(AppContext);
+  const { user } = state;
+  console.log("Main.js state", state);
   return (
     <div className="App  ">      
        
-      
+        {(!user.name || !user.expertTag) && <Alert type="warning" text="Please, complete the Profile!" link="/Profile" />}
         <div className="white-bg container border border-1 p-2 ">
                 <Questions />
         </div>               
